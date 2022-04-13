b528050f16e87e34a9ec45d56c3e140d5240b56b
Debojyoti Das <debojyoti.das@thermofisher.com>
2022-04-13 16:58:52 +0530

    After 2nd Round Interview Update

--- solution/gencsv.sh
+++ solution/gencsv.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 RANDOM=$$
-count=0
+count=1
 rm -rf inputFile
 touch inputFile
 # Number of Count
+if [ -z $1 ]; then
 until [ $count -gt 10 ]
 do
 echo "$count, $((RANDOM%390))" >> inputFile
 ((count=count+1))
 done
+else
+until [ $count -gt $1 ]
+do
+echo "$count, $((RANDOM%390))" >> inputFile
+((count=count+1))
+done
+fi
